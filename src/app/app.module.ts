e033dbce44f7d9fc343fe813f915f7fe20107e85
gmwendt <marcon@elipse.com.br>
2017-05-17 09:18:44 -0300

    Added angularfire module, created auth service. Next step 'Implement LoginPage Component'.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,30 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AngularFireModule} from 'angularfire2';
 
 import { AppComponent }  from './app.component';
 
+import { AuthService } from './providers/auth.service';
+
+export const firebaseConfig = {
+    apiKey: "<your-key>",
+    authDomain: "<your-project-authdomain>",
+    databaseURL: "<your-database-url>",
+    storageBucket: "<your-storage-bucket>",
+    messagingSenderId: "<your-messaging-sender-id>"
+  };
+
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports: [ 
+  	BrowserModule, 
+  	FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
   declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  providers: [],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
